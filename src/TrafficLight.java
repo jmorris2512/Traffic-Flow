d880fe3a792497769276a3ba0c10c8a32fcb4ab9
jmorris2512 <justinmorris2512@gmail.com>
2025-04-30 17:08:09 -0400

    Update TrafficLight.java

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,16 +1,35 @@
-public class TrafficLight {
-    private String color;
+public enum TrafficLightColor {
+    RED, GREEN, YELLOW;
+}
 
-    public TrafficLight(String color) {
+public class TrafficLight {
+    private TrafficLightColor color;
+
+    public TrafficLight(TrafficLightColor color) {
         this.color = color;
     }
 
-    public void changeColor(String newColor) {
+    public void changeColor(TrafficLightColor newColor) {
         this.color = newColor;
         System.out.println("Traffic light changed to: " + color);
     }
 
-    public String getColor() {
+    public TrafficLightColor getColor() {
         return this.color;
+    }
+
+    // Simulate the traffic light cycling
+    public void cycle() {
+        switch (color) {
+            case RED:
+                changeColor(TrafficLightColor.GREEN);
+                break;
+            case GREEN:
+                changeColor(TrafficLightColor.YELLOW);
+                break;
+            case YELLOW:
+                changeColor(TrafficLightColor.RED);
+                break;
+        }
     }
 }
