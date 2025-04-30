c8d0665f32f896fa03c66b4cec8b908fc9f62be7
jmorris2512 <justinmorris2512@gmail.com>
2025-04-30 17:09:43 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Initialize Traffic Lights
-        TrafficLight light1 = new TrafficLight("Red");
-        TrafficLight light2 = new TrafficLight("Green");
+        // Initialize Traffic Lights with Enum
+        TrafficLight light1 = new TrafficLight(TrafficLightColor.RED);
+        TrafficLight light2 = new TrafficLight(TrafficLightColor.GREEN);
 
         // Initialize Cars
         Car car1 = new Car("Toyota", "Corolla", 30);
         Car car2 = new Car("Honda", "Civic", 40);
 
-        // Initialize Simulation
+        // Initialize Simulation Controller
         SimulationController controller = new SimulationController();
         controller.addTrafficLight(light1);
         controller.addTrafficLight(light2);
