46627bf70505df35bc1631c91459becb90c5909c
jmorris2512 <justinmorris2512@gmail.com>
2025-04-30 17:14:55 -0400

    Update SimulationController.java

--- src/SimulationController.java
+++ src/SimulationController.java
@@ -4,10 +4,14 @@ import java.util.List;
 public class SimulationController {
     private List<TrafficLight> trafficLights;
     private List<Car> cars;
+    private int trafficDensity;  // Represents the number of cars on the road
+    private boolean incidentOccurred;
 
-    public SimulationController() {
+    public SimulationController(int trafficDensity) {
         this.trafficLights = new ArrayList<>();
         this.cars = new ArrayList<>();
+        this.trafficDensity = trafficDensity;
+        this.incidentOccurred = false;  // No incident by default
     }
 
     public void addTrafficLight(TrafficLight light) {
@@ -18,25 +22,39 @@ public class SimulationController {
         cars.add(car);
     }
 
-    public void runSimulation() {
-        // Simulate a simple loop with traffic lights changing and cars reacting
-        int cycleCount = 5; // Number of cycles to simulate
+    public void simulateIncident() {
+        // Randomly simulate incidents (e.g., stalled car or accident)
+        if (Math.random() < 0.1) { // 10% chance of incident
+            this.incidentOccurred = true;
+            System.out.println("An incident occurred: Lane blocked by a stalled car!");
+        }
+    }
 
+    public void runSimulation(int cycleCount) {
         for (int i = 0; i < cycleCount; i++) {
             System.out.println("\nCycle " + (i + 1) + " starts:");
 
-            // Change traffic light state
+            // Simulate incident (10% chance)
+            simulateIncident();
+
+            // Adjust traffic light based on traffic density
             for (TrafficLight light : trafficLights) {
-                System.out.println("Current light color: " + light.getColor());
-                light.cycle(); // Cycle the light
+                light.adjustSignalDuration(trafficDensity);
+                light.cycle(); // Cycle traffic light (change colors)
             }
 
-            // Cars respond to traffic lights
+            // Cars react based on traffic light status and incidents
             for (Car car : cars) {
-                if (trafficLights.get(0).getColor() == TrafficLightColor.GREEN) {
-                    car.accelerate(10); // Cars accelerate on green light
+                if (incidentOccurred) {
+                    // Reduce car speed if there's an incident
+                    car.brake(5);  // Cars slow down due to the incident
                 } else {
-                    car.brake(5); // Cars brake on red/yellow light
+                    // Normal car behavior based on light status
+                    if (trafficLights.get(0).getColor() == TrafficLightColor.GREEN) {
+                        car.accelerate(10);  // Accelerate on green
+                    } else {
+                        car.brake(5);  // Brake on red/yellow
+                    }
                 }
             }
         }
@@ -44,4 +62,3 @@ public class SimulationController {
         System.out.println("Simulation has completed.");
     }
 }
-
